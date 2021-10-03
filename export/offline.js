e015e59054222bb0194990412092cb6a3b50f050
lishane31 <sil218@ucsd.edu>
2021-10-03 12:11:20 -0700

    Add files

---
+++ export/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1633288023,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/blaster-sheet0.png",
+		"images/blue-sheet0.png",
+		"images/red-sheet0.png",
+		"images/yellow-sheet0.png",
+		"images/green-sheet0.png",
+		"images/darkblue-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/spawner-sheet0.png",
+		"images/black-sheet0.png",
+		"images/white-sheet0.png",
+		"images/sphere-sheet0.png",
+		"images/ai-sheet0.png"
+	]
+}
